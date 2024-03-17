565da48095fd3835137743772b05a6ad48194cd1
Paulo Mário Valente Bumba <143729787+PauloBumba@users.noreply.github.com>
2024-03-17 19:51:42 -0300

    update

---
+++ Atividades/Class/Print.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    public class Print
+    {
+        public void print(){
+             Console.Write("Paulo Bumba");
+        }
+    }
+}
