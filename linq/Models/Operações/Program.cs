7bf0787f6e8131bbf03cbe38fc65088e3bef486b
Paulo Mário Valente Bumba <143729787+PauloBumba@users.noreply.github.com>
2024-04-29 21:21:37 -0300

    Update

---
+++ linq/Models/Operações/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+int [] vetor= {
+    1,2,3,4,5,6,7,8,9,0,12,23,12,3,4
+};
+
+var buscarPare = from c in vetor
+                where c==2
+                select c;
+
+foreach(var c in buscarPare){
+    Console.WriteLine($"No vetor {c} se encotra na posição{c}");
+}
